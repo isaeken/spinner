0cc1d5c9670e87585f3dd805ae41953f409249dc
Isa Eken <hello@isaeken.com.tr>
2021-07-24 23:15:09 +0300

    bugfix: try to find autoloader in parent directories if not exists.

--- bin/spin.php
+++ bin/spin.php
@@ -1,9 +1,18 @@
+#!/usr/bin/env php
 <?php
 
 use IsaEken\Spinner\Helpers;
 use IsaEken\Spinner\LockFile;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+}
+else if (file_exists(__DIR__ . '/../../../autoload.php')) {
+    require_once __DIR__ . '/../../../autoload.php';
+}
+else {
+    throw new Exception('Autoloader not found. Maybe you forgot to run the "composer install" command?');
+}
 
 /**
  * Create the lock file instance.
